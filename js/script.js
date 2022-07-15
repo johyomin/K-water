28a65d796984639d724182f5d4e2593acfe096e4
hyomin <johyomin1119@gmail.com>
2022-07-16 01:25:59 +0900

    service section complete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {});
 window.onload = function () {
-  // 랜덤 천사 기능(0~2)
   let rNum = Math.floor(Math.random() * 3);
   let rClass = 'about-box-char-' + rNum;
   let rTag = $('.about-box-sns');
   rTag.addClass(rClass);
 
-  // 메뉴 기능
   let header = $('.header');
   let gnb = $('.gnb');
   let gnbH = gnb.height();
@@ -18,7 +16,6 @@ window.onload = function () {
     header.css('height', 70);
   });
 
-  // 비주얼 슬라이드
   new Swiper('.sw-visual', {
     slidesPerView: 8,
     loop: true,
@@ -28,7 +25,6 @@ window.onload = function () {
     }
   });
 
-  // about 슬라이드
   let swAbout = new Swiper('.sw-about', {
     loop: true,
     autoplay: {
@@ -49,19 +45,12 @@ window.onload = function () {
 
   let swAboutBt = $('.sw-about-pause');
   swAboutBt.click(function () {
-    // 현재 class 상태 체크
     let temp = $(this).hasClass('sw-about-play');
     if (temp == true) {
-      // 슬라이드 자동 실행
       swAbout.autoplay.start();
-      // 아이콘을 pause 버튼으로 바꾼다.
-      // 사용자는 멈추기 위해서 클릭을 하도록 안내한다.
       $(this).removeClass('sw-about-play');
     } else {
-      // 슬라이드 멈춤
       swAbout.autoplay.stop();
-      // 아이콘을 play 버튼으로 바꾼다.
-      // 사용자는 멈춘 슬라이드를 play 하려고 할것이다.
       $(this).addClass('sw-about-play');
     }
   });
